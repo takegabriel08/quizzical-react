f9b0db06c6897946993931c1d9be22d2366cd3ce
Gabriel <Gabriel@DESKTOP-7AUAOTD>
2023-02-09 00:52:54 +0200

    Created quizz page design without logic

--- src/components/QuizQuestion.jsx
+++ src/components/QuizQuestion.jsx
@@ -5,18 +5,33 @@ export default function QuizQuestion(props) {
     console.log('quiz question', props)
     // console.log('answers', answers)
 
-    const answers = [...props.incorrect_answers, correct_answer]
-    function randomizeAnswers(){
-        answers.map()
+    const answers = [...props.incorrect_answers, props.correct_answer]
+    function randomizeAnswers(arr) {
+        const scramble = []
+        let randomIdx = Math.floor(Math.random() * 4);
+
+        arr.map((el, idx) => {
+            if (randomIdx + 1 > arr.length - 1) {
+                scramble[randomIdx] = el;
+                randomIdx = 0
+            } else {
+                scramble[randomIdx] = el;
+                randomIdx++
+            }
+        })
+        return scramble;
     }
+    const answerElements = randomizeAnswers(answers).map(el => {
+        return (
+            <h3 className='quiz-option' >{el.replace(/&#039;/g, "'").replace(/&quot;/g, '"').replace(/&amp;/g, '&')}</h3>
+        )
+    })
+
     return (
         <div className="container">
-            <h1 className='quiz-question'>{props.question}</h1>
+            <h1 className='quiz-question'>{props.question.replace(/&#039;/g, "'").replace(/&quot;/g, '"').replace(/&amp;/g, '&')}</h1>
             <div className="options-container">
-                <h3 className='quiz-option' >no</h3>
-                <h3 className='quiz-option clicked' >yes</h3>
-                <h3 className='quiz-option' >why</h3>
-                <h3 className='quiz-option' >ask</h3>
+                {answerElements}
             </div>
             <hr style={{ marginTop: "1em" }} />
         </div>
