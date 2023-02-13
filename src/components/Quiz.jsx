c0b2a212e8b58217fa5a597b4e742616e2e6564b
Gabriel <Gabriel@DESKTOP-7AUAOTD>
2023-02-14 00:49:28 +0200

    logic for game end and restart but no new question

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,6 @@
+import { nanoid } from 'nanoid'
+import { useState, useEffect } from 'react'
 import '../App.css'
-import QuizQuestion from './QuizQuestion'
 
 export default function Quiz(props) {
     // console.log(props)
@@ -8,19 +9,122 @@ export default function Quiz(props) {
         return new DOMParser().parseFromString(str, 'text/html').body.textContent
     }
 
-    const quizQuestionElements = props.data.map((el, idx) => {
+    function prepareQuestions(data) {
+        return data.map(data => {
+            return {
+                question: data.question,
+                questionId: nanoid(),
+                correctAnswer: data.correct_answer,
+                answers: [...data.incorrect_answers, data.correct_answer].map(answer => {
+                    return {
+                        text: decodePartialStr(answer),
+                        isSelected: false,
+                        answerId: nanoid(),
+                        isCorrect: answer == data.correct_answer
+                    }
+                })
+            }
+        })
+    }
+
+    const [questions, setQuestions] = useState(prepareQuestions(props.data))
+    const [gameEnd, setGameEnd] = useState(false)
+    const [canEnd, setCanEnd] = useState(false)
+
+    useEffect(() => {
+        function isSelected(element, index, array) {
+            return element.isSelected;
+        }
+        const isTrue = (currentValue) => currentValue == true;
+        let canEndGame = []
+        for (let index = 0; index < questions.length; index++) {
+            const element = questions[index];
+            canEndGame.push(element.answers.some(isSelected))
+        }
+        setCanEnd(canEndGame.length == questions.length && canEndGame.every(isTrue))
+    }, [questions])
+
+    function selectOption(optionId) {
+        const toUpdate = questions.map(q => {
+            return {
+                ...q,
+                answers: q.answers.map(answer => {
+                    if (answer.answerId == optionId) {
+                        return { ...answer, isSelected: !answer.isSelected }
+                    } else if (q.answers.includes(q.answers.filter(el => el.answerId == optionId)[0])) {
+                        return { ...answer, isSelected: false }
+                    } else {
+                        return answer
+                    }
+                })
+            }
+        })
+        setQuestions(questions => toUpdate)
+    }
+
+    function showResults() {
+        console.log(questions)
+
+        if (!gameEnd && canEnd) {
+            console.log("end the game")
+            setGameEnd(true)
+        }
+        if (gameEnd) {
+            setGameEnd(false)
+            setQuestions(prepareQuestions(props.data))
+        }
+    }
+
+    const qs = questions.map(questionEl => {
+        const answers = questionEl.answers.map(answer => {
+            let answerClass = answer.isSelected ? 'answer-option selected' : 'answer-option'
+            if (gameEnd && answer.isSelected && !answer.isCorrect) {
+                answerClass = `${answerClass} incorrect`
+            }
+            if (gameEnd && answer.isCorrect) {
+                answerClass = `${answerClass} correct`
+            }
+            return (
+                <h4
+                    key={answer.answerId}
+                    className={answerClass}
+                    onClick={() => { selectOption(answer.answerId) }}
+                >
+                    {answer.text}
+                </h4>
+            )
+        })
         return (
-            <QuizQuestion
-                {...el}
-                decode={decodePartialStr}
-                key={idx}
-            />
+            <div className="question" key={questionEl.questionId}>
+                <h2 className='quiz-question'>{decodePartialStr(questionEl.question)}</h2>
+                <div className="options-container">
+                    {answers}
+                </div>
+                <hr />
+            </div>
         )
     })
+
+
+    const CheckAnswerBtn = () => {
+        var checkAnswersClass = "start-quizz"
+        if (!canEnd) {
+            checkAnswersClass = `${checkAnswersClass} disabled`
+        }
+        return (
+            <div
+                className={checkAnswersClass}
+                onClick={checkAnswersClass.includes('disabled') ? null : showResults}
+            >
+                {!gameEnd ? 'Check answers' : 'Play again'}
+            </div>
+        )
+    }
     return (
         <div className="quiz">
-            {quizQuestionElements}
-            <div className="start-quizz fixed">Check answers</div>
+            {qs}
+            <CheckAnswerBtn />
         </div>
     )
+
 }
